9c459940990e962dd0b5ce768c09362d5101ddc6
ekioemman@gmail.com <ekioemman@gmail.com>
2022-09-13 20:02:35 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,13 @@
 #include "main.h"
 /**
  * add - adds two integers and return their result
+ *
  * @x: number being added
+ * 
  * @y: number being added
+ * 
  * return: Always 0.
+ *
  */
 int add(int x, int y)
 {
