8a8f3a4d929d153d005a8db57c547507f80f0dae
ekioemman@gmail.com <ekioemman@gmail.com>
2022-09-19 13:19:46 -0700

    task 11 corrected

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 /**
  * main - generates random valid passwords
  * Return: 0 (on success).
+ *
  *
  */
 int main(void)
