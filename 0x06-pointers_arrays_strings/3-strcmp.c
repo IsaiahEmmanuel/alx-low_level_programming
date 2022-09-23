7caa4c38adb0157c85e667ca3f69024d0122662b
ekioemman@gmail.com <ekioemman@gmail.com>
2022-09-23 14:09:43 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmp - compar
+ * _strcmp - compare
  * @s1 : pointerto char params
  * @s2 : pointer to char params
  * Return: *dest
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int R;
-	
+
 	i = 0;
 
 	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
