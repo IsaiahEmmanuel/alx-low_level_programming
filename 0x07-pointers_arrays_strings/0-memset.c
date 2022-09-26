d1bfba88097ba12c37acae43d57fff81bb6691ed
ekioemman@gmail.com <ekioemman@gmail.com>
2022-09-26 11:41:53 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,17 +6,14 @@
  *@n: bytes of the memory
  *Return: dest
  */
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
 	unsigned int i;
 
 	/*Delacring FOR*/
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b; /*add 1 position s*/
-
+		s[i] = b; /*add 1 position s*/
 	} /*END FOR*/
 
 	return (s);
